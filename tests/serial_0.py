36a4287e5021d22a0671f59ee6dc988fb41cf2e0
EchoFUN <xukai.ken@gmail.com>
2017-01-15 22:21:08 +0800

    Add

--- tests/serial_0.py
+++ tests/serial_0.py
@@ -20,20 +20,17 @@ import serial
 
 sensor = serial.Serial(port='/dev/ttyAMA0', baudrate='9600', timeout=1)
 
+def convert(hexVal):
+    return int(codecs.encode(hexVal, 'hex'), 16)
+
 while True:
     data = sensor.read(size=1)
     if (data == b'\x55'):
         print('Get the data !')
         sensor.read(size=10)
-
         break
 
     print('trying', data)
-
-
-def convert(hexVal):
-    return int(codecs.encode(hexVal, 'hex'), 16)
-
 
 try:
     while True:
@@ -42,31 +39,40 @@ try:
             print('Byte error !')
             break
 
+        if data[1] == b'\x50':
+
+            print('Time is ')
+
+        '''
         if data[1] == b'\x54':
 
             x = convert(data[2:4])
             y = convert(data[4:6])
             z = convert(data[6:8])
 
-            print("Magnetic output:{}, {}, {}".format(x, y, z))
+            # print("Magnetic output:{}, {}, {}".format(x, y, z))
 
         #Angle
-        elif data[1] == 83:
+        
+        elif data[1] == b'\x53':
+            print(data[2:4])
+            # exit()
+
             x = convert(data[2:4]) / 32768 * 180
             y = convert(data[4:6]) / 32768 * 180
             z = convert(data[6:8]) / 32768 * 180
 
             print("Angle output:{}, {}, {}".format(x, y, z))
 
-        #Acceleration
         elif data[1] == b'\x51':
             x = convert(data[2:4]) / 32768 * 16
             y = convert(data[4:6]) / 32768 * 16
             z = convert(data[6:8]) / 32768 * 16
 
-            print("Acceleration output:{}, {}, {}".format(x, y, z))
+            print("Acceleration output: {}, {}, {}".format(x, y, z))
+        '''
 
-        print("----", data[0], data[1])
+        # print("----", data[0], data[1])
 
 except KeyboardInterrupt:
     sensor.close()
