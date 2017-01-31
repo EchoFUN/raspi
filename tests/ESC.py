4ef031373f2ddaa07a0914164ef6f85f976aaa5e
EchoFUN <xukai.ken@gmail.com>
2017-02-01 01:34:44 +0800

    Add

--- tests/ESC.py
+++ tests/ESC.py
@@ -17,11 +17,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 
-p = GPIO.PWM(7, 50) # 500Hz，每个信号周期为 1000 / 500 = 2ms； dutyCycle为50%，则为1ms(1000us)最低油门，100%为2ms(2000us)最高油门
+p = GPIO.PWM(7, 50) # 50Hz，每个信号周期为 1000 / 50 = 20ms； dutyCycle为5，则为1ms(1000us)最低油门；10为2ms(2000us)最高油门
 p.start(5)
 time.sleep(2)
 
-p.ChangeDutyCycle(5.5)
+p.ChangeDutyCycle(5.5) # 油门大约在 10%的样子
 
 raw_input('Press return to stop !')
 p.stop()
