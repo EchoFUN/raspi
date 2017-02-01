bad1b3d971db3282ce777cb721ac993a55b4974f
EchoFUN <xukai.ken@gmail.com>
2017-02-01 17:11:54 +0800

    Add

--- tests/ESC.py
+++ tests/ESC.py
@@ -15,10 +15,10 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
 p = GPIO.PWM(
-    18, 400
+    4, 400
 )  # 400Hz，每个信号周期为 1000 / 400 = 2.5ms； dutyCycle为40，则为1ms(1000us)最低油门；80为2ms(2000us)最高油门
 p.start(40)
 time.sleep(2)
