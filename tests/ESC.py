3d6884bbb2839288bab140ceb090184da9d9dce0
EchoFUN <xukai.ken@gmail.com>
2017-02-01 15:40:43 +0800

    DEBUG the PWM signal function.

--- tests/ESC.py
+++ tests/ESC.py
@@ -14,14 +14,16 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-p = GPIO.PWM(7, 50) # 50Hz，每个信号周期为 1000 / 50 = 20ms； dutyCycle为5，则为1ms(1000us)最低油门；10为2ms(2000us)最高油门
-p.start(5)
+p = GPIO.PWM(
+    18, 400
+)  # 400Hz，每个信号周期为 1000 / 400 = 2.5ms； dutyCycle为40，则为1ms(1000us)最低油门；80为2ms(2000us)最高油门
+p.start(40)
 time.sleep(2)
 
-p.ChangeDutyCycle(5.5) # 油门大约在 10%的样子
+p.ChangeDutyCycle(45)  # 油门大约在 10%的样子
 
 raw_input('Press return to stop !')
 p.stop()
---
+++ tests/ESC_1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+#
+# @author XU Kai(xukai.ken@gmail.com)
+# @date 2016-12-04 星期日
+#
+#
+# #fileOverview 使用 pigpio hardware PWM . 来解决精度不准的问题
+#
+#
+#
+
+import time
+import pigpio
+
+PIN_CODDE = 4
+
+pi = pigpio.pi()
+if not pi.connected:
+    exit(0)
+
+# 第二个参数指代了PWM信号脉宽，1000us的脉宽表示了0%的油门，2000us的脉宽表示了100%的油门。
+# 1150 指代了约 （1150 - 1000）/ 1000 * 100% = 15% 的油门。
+pi.set_servo_pulsewidth(PIN_CODDE, 1150)
+
+try:
+
+    time.sleep(1000)
+except KeyboardInterrupt:
+
+    pi.set_servo_pulsewidth(PIN_CODDE, 0)
+    pi.stop()
