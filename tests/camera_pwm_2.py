2a7dc87c0638b082f2a143ba7aa60d5dadcfa7d2
EchoFUN <xukai.ken@gmail.com>
2017-02-03 22:06:43 +0800

    Add

---
+++ tests/camera_pwm_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+#
+# @author XU Kai(xukai.ken@gmail.com)
+# @date 2016-12-04 星期日
+#
+#
+# #fileOverview 树莓派自带 RPi.GPIO 库 PWM 信号不准确
+#
+#
+#
+
+# PWM控制信号周期20ms, 脉宽 0.5ms-2.5ms 对应的角度-90到+90度, 范围180度(3度左右偏差), 当脉宽1.5ms时舵机在中立点(0度);
+
+import time
+import pigpio
+
+PIN_CODDE = 12
+
+pi = pigpio.pi()
+if not pi.connected:
+    exit(0)
+
+pi.set_servo_pulsewidth(PIN_CODDE, 1500)
+
+pi.stop()
