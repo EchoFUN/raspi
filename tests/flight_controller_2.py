e679a86fb4f37abae2b828aafe575df76f6147ac
EchoFUN <xukai.ken@gmail.com>
2019-06-21 23:41:00 +0800

    ADD

--- tests/flight_controller_2.py
+++ tests/flight_controller_2.py
@@ -13,14 +13,16 @@
 import time
 import pigpio
 
-pi = pigpio.pi()
+pi = pigpio.pi('127.0.0.1', '8888')
 if not pi.connected:
     exit(0)
+
+print('connected to the pi ')
 
 pi.set_servo_pulsewidth(12, 1900)
 pi.set_servo_pulsewidth(16, 1000)
 
-time.sleep(2)
+time.sleep(3)
 pi.set_servo_pulsewidth(12, 1500)
 
 try:
